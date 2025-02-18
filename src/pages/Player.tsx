861cbf5d4bdb967c939ad89fb4c4d3ed19620efa
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-18 15:23:21 -0300

    feat: Set all the data based in the zustand functions of load the data that is take at the database and change components based in that data

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -2,8 +2,29 @@ import { MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { VideoPlayer } from "../components/VideoPlayer";
 import { Module } from "../components/Module";
+import { useEffect } from "react";
+import { useCurrentLesson, useStore } from "../zustand-store";
 
 export function Player() {
+    const { course, load } = useStore(store => {
+        return {
+            course: store.course,
+            load: store.load
+        }
+    })
+
+    const { currentLesson } = useCurrentLesson()
+
+    useEffect(() => {
+        load()
+    }, []);
+
+    useEffect(() => {
+        if (currentLesson) {
+            document.title = `Assistindo: ${currentLesson.title}`
+        }
+    }, [currentLesson]);
+
     return (
         <div className="h-screen bg-zinc-900 text-zinc-50 flex justify-center items-center">
             <div className="flex w-[1100px] flex-col gap-6">
@@ -22,9 +43,16 @@ export function Player() {
                     </div>
 
                     <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll">
-                        <Module title="Fundamentos do Redux" lessonsAmount={3} moduleIndex={0} />
-                        <Module title="Fundamentos do Redux" lessonsAmount={3} moduleIndex={1} />
-                        <Module title="Fundamentos do Redux" lessonsAmount={3} moduleIndex={2} />
+                        {course?.modules && course?.modules.map((module, index) => {
+                            return (
+                                <Module 
+                                    key={module.id}
+                                    title={module.title} 
+                                    lessonsAmount={module.lessons.length} 
+                                    moduleIndex={index} 
+                                />
+                            )
+                        })}
                     </aside>
                 </main>
 
