91dfee359a94c158984fdedf057729c4129c6a6f
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-18 15:18:25 -0300

    feat: Removed the Redux provider to use zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { Provider as ReduxProvider } from 'react-redux';
-import { store } from "./store/storeRedux";
 import { Player } from './pages/Player';
 
 export function App() {
   return (
-    <ReduxProvider store={store} >
       <Player />
-    </ReduxProvider>
   )
 }
