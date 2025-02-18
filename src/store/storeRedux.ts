75e79650085151b9f2c586b9dd82bcf747d5302c
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-18 15:16:26 -0300

    feat: Added the player slice to store configuration and created a personalized dispatch

--- src/store/storeRedux.ts
+++ src/store/storeRedux.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { TypedUseSelectorHook, useSelector } from 'react-redux'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import { player } from '../slices/player'
 
 export const store = configureStore({
     reducer: {
+        player
     }
 })
 
 export type RootType = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 
 export const useAppSelector: TypedUseSelectorHook<RootType> = useSelector
+export const useAppDispatch: () => AppDispatch = useDispatch
