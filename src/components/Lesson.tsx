c7079e4bcfbff4e0eb8c1197b5282b14db4b506c
leonardobolfarini <leonardobolfarini@gmail.com>
2025-02-18 15:21:38 -0300

    feat:Created component props to change the lesson and style the lessons

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,14 +1,23 @@
-import { Video } from "lucide-react";
+import { PlayCircle, Video } from "lucide-react";
 
 interface LessonProps {
     title: string
     time: string
+    onChangeLesson: () => void
+    isCurrent?: boolean
 }
 
-export function Lesson({ time, title }: LessonProps) {
+export function Lesson({ time, title, onChangeLesson, isCurrent = false }: LessonProps) {
     return (
-        <button className="flex items-center gap-3 text-sm text-zinc-400">
-            <Video className="w-4 h-4 text-zinc-500" />
+        <button 
+            onClick={onChangeLesson}
+            data-active={isCurrent}
+            disabled={isCurrent}
+            className="flex items-center gap-3 text-sm text-zinc-400 data-[active=true]:text-emerald-400 enabled:hover:text-zinc-100"
+        >
+            {isCurrent ? (
+                <PlayCircle className="w-4 h-4 text-emerald-400"/>
+            ) : ( <Video className="w-4 h-4 text-zinc-500" /> )}
             <span>{title}</span>
             <span className="ml-auto font-mono text-sm text-zinc-500">{time}</span>
         </button>
